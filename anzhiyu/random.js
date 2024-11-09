f9450b3ea728cccec07b7f8443fc4be1cbb44952
sink55 <sinktf74@gmail.com>
2024-11-09 15:39:14 +0800

    Site updated: 2024-11-09 15:39:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/09/hello-world/","2024/11/09/My-New-Post/"];function toRandomPost(){
+var posts=["2024/11/09/My-New-Post/","2024/11/09/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
